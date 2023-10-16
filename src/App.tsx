b714bcb65c6f949375570c7280385cc80f618011
Roman Gaspar <romanalejandro99@hotmail.com>
2023-10-16 15:33:44 -0700

    subs are now shown

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,35 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [number, setNumber] = useState <string | number>(5)
+  const [subs, setSubs] = useState(
+    [{
+      nick: 'name',
+      subMonth: 3,
+      avatar: 'https://i.pravatar.cc/150?u=name',
+      description: 'The first sub'
+    },
+    {
+      nick: 'name2',
+      subMonth: 1,
+      avatar: 'https://i.pravatar.cc/150?u=name2',
+    }]
+  )
 
-  const changeNumber = ()=>{
-    setNumber("2")
-  }
+
   return (
     <div className="App">
-     {number}
-     <button onClick={changeNumber}> change number</button>
+      <h1>subs</h1>
+      {
+        subs.map(sub =>{
+          return(
+            <li key={sub.nick}>
+              <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+              <h4>{sub.nick} (<small>{sub.subMonth}</small>)</h4>
+              <p>{sub.description?.substring(0,100)}</p>
+            </li>
+          )
+        })
+      }
     </div>
   );
 }
